6381be39061154672c721bcd02046d4bd3a7d508
David Robertson <davidrobertson@MacBook-Pro-2.local>
2021-03-06 14:18:25 -0500

    File I/O working

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -13,11 +18,48 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("."));
+        File mainDirectory = directoryChooser.showDialog(primaryStage);
+
+
+        File dataDir = mainDirectory;
+        File outFile = new File("../Assignment-1/output");
+
+        FileReader wordCounter = new FileReader();
+        System.out.println("Hello");
+        try{
+            wordCounter.parseFile(dataDir);
+            wordCounter.outputWordCount(2, outFile);
+        }catch(FileNotFoundException e){
+            System.err.println("Invalid input dir: " + dataDir.getAbsolutePath());
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
+
+
+
+        /*
+        if(args.length < 2){
+            System.err.println("Usage: java WordCounter <inputDir> <outfile>");
+            System.exit(0);
+        }*/
+
+
+
+
         launch(args);
     }
+
+
 }
